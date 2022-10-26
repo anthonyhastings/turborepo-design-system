5ee381f83fc359205e052a69de65dfb06e9b78fe
Anthony Hastings <ar.hastings@gmail.com>
2022-10-26 22:02:26 +0100

    feat(tds-core): using Vite + TSC for build tooling (#18)

--- packages/tds-core/vite.config.ts
+++ packages/tds-core/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: './src/index.tsx',
+      formats: ['cjs', 'es'],
+    },
+    rollupOptions: {
+      external: ['react'],
+    },
+    sourcemap: true,
+    target: 'es2019',
+  },
   plugins: [react()],
+  publicDir: false,
 });
