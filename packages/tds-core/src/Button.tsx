4c978fbd2f173c29e45a02d3a0615adbb328299b
Franz Bendezu <franzant20@gmail.com>
2023-10-29 18:18:17 -0500

    feat: support type button

--- packages/tds-core/src/Button.tsx
+++ packages/tds-core/src/Button.tsx
@@ -1,10 +1,15 @@
 export interface ButtonProps {
   children: React.ReactNode;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  type?: 'button' | 'submit' | 'reset';
 }
 
-export function Button({ children, onClick }: ButtonProps) {
-  return <button onClick={onClick}>{children}</button>;
+export function Button({ children, onClick, type }: ButtonProps) {
+  return (
+    <button type={type} onClick={onClick}>
+      {children}
+    </button>
+  );
 }
 
 Button.displayName = 'Button';
